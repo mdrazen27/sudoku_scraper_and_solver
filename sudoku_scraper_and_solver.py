3285a56c5cafe773756962ce9dd856e0f9cc821e
Drazen Minic <mdrazen27@gmail.com>
2022-03-13 20:43:14 +0100

    solution to sudoku using backtracking

--- sudoku_scraper_and_solver.py
+++ sudoku_scraper_and_solver.py
@@ -2,6 +2,7 @@
 Sudoku scraping and solving along with interactive Gui
 """
 
+from copy import deepcopy
 from tkinter import *
 from bs4 import BeautifulSoup
 import requests
@@ -29,6 +30,10 @@ class SudokuScraperAndSolver:
                 sudoku[row][collumn] = int(line["value"])
             collumn += 1
         self.sudoku = sudoku
+        cpy = deepcopy(sudoku)
+        self.backtracking(cpy)
+        self.solved = cpy
+
 
     def __str__(self):
         return str(self.sudoku)
@@ -36,3 +41,47 @@ class SudokuScraperAndSolver:
     def __repr__(self):
         return f'{self.sudoku}'
 
+    def check_row_col_block(self,num,position,sudoku):
+        """
+        checks if number conflicts with one already existing in the same row, column or block
+        """
+        for i in range(9):
+            if sudoku[i][position[1]] == num and i != position[0]:
+                return False
+            if sudoku[position[0]][i] == num and i != position[1]:
+                return False
+        for i in range(position[0]//3*3, position[0]//3*3+3):
+            for j in range(position[1]//3*3,position[1]//3*3+3):
+                if (i,j)!=position and num == sudoku[i][j]:
+                    return False
+        return True
+
+    def see_next_empty(self,sudoku):
+        """checks if any of the blocks is still empty"""
+        for i in range(9):
+            for j in range(9):
+                if sudoku[i][j] == 0:
+                    return (i, j)
+        return False
+
+    def backtracking(self,sudoku):
+        """
+        if all fields are filled program stops if not it fills all available cells untill conflict
+        occurs when it occurs it goes on previos number untill conflict is resolved
+        """
+        if self.see_next_empty(sudoku):
+            k,m = self.see_next_empty(sudoku)
+        else:
+            return True
+        for num in range(1,10):
+            if self.check_row_col_block(num,[k,m],sudoku):
+                sudoku[k][m] = num
+                if self.backtracking(sudoku):
+                    return True
+                sudoku[k][m] = 0
+        return False
+
+
+sudoku_scraped = SudokuScraperAndSolver(3)
+print(sudoku_scraped.sudoku)
+print(sudoku_scraped.solved)
