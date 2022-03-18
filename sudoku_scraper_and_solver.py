d4cc4576e24cfc62968a245e73199e8fbb8f95a5
Drazen Minic <mdrazen27@gmail.com>
2022-03-18 20:10:49 +0100

    removed excess

--- sudoku_scraper_and_solver.py
+++ sudoku_scraper_and_solver.py
@@ -1,6 +1,4 @@
-"""
-Sudoku scraping and solving along with interactive Gui
-"""
+"""Sudoku scraping and solving"""
 
 from copy import deepcopy
 from datetime import datetime
@@ -83,39 +81,11 @@ class SudokuScraperAndSolver:
         return False
 
     def saving_solution_to_txt(self,solution):
+        """saves solution in database with unique time of solution"""
         time =datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-        file_name = "./db/automatic_result_"+time+".txt"
+        file_name = "./db/automatic-result-"+time+".txt"
         with open(file_name,'w', encoding = 'utf-8') as file:
             for line in solution:
                 file.writelines(str(line)+"\n")
 
-""" sudoku_scraped = SudokuScraperAndSolver(3)
-print(sudoku_scraped.sudoku)
-print(sudoku_scraped.solved) """
-""" 
-class Root(Tk):
-    def __init__(self,sudoku):
-        super(Root,self).__init__()
-        self.title("Sudoku")
-        self.minsize(400,400)
-        self.make_gameboard(sudoku)
-
-    def write_entry_into_board(self,i,j,sudoku):
-        sudoku[i][j] = int(entry_field.get())
-
-    def make_gameboard(self,sudoku):
-        for i in range(9):
-            for j in range(9):
-                if sudoku[i][j] != 0:
-                    already_set = Label(self,text=sudoku[i][j],font=("Arial",18))
-                    already_set.grid(row=i,column=j)
-                else:
-                    entry_field = Entry(self,width=3,font=("Arial",18))
-                    entry_field.bind("<KeyRelease>",self.write_entry_into_board(i,j,sudoku))
-                    entry_field.grid(row=i,column=j)
-
-                    sudoku[i][j] = int(entry_field.get())
-
-
-root = Root(sudoku_scraped.sudoku)
-root.mainloop() """
+sudoku_scraped_start = SudokuScraperAndSolver(3)
